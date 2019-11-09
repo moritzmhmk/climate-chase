eedcd5367102c9ff29351b985c19072e63cb2201
moritzmhmk <moritzmhmk@gmail.com>
2019-11-09 15:46:36 +0100

    added react-simple-maps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { ComposableMap, Geographies, Geography, Marker, Line } from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ComposableMap width={960} projection="geoMercator" style={{background: "#556270"}}>
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map(geo => <Geography key={geo.rsmKey} stroke="none" fill="#C7F464" geography={geo} />)
+        }
+      </Geographies>
+      <Line
+        from={[2.3522, 48.8566]}
+        to={[-74.006, 40.7128]}
+        stroke="#4ECDC4"
+        strokeWidth={2}
+        strokeLinecap="round"
+      />
+      <Marker coordinates={[-74.006, 40.7128]}>
+        <circle r={4} fill="#C44D58" />
+      </Marker>
+    </ComposableMap>
   );
 }
 
