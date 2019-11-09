1e30ba80c2e374cd70d407ed10fc2f7ecbf2b07d
moritzmhmk <moritzmhmk@gmail.com>
2019-11-09 19:00:46 +0100

    travel the world

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import { ComposableMap, Geographies, Geography, Marker, Line } from "react-simple-maps";
 
@@ -7,99 +7,123 @@ import UserView from './UserView'
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-const airports = [
-  {
-    name: "Berlin (Schoenefeld)",
-    id: "SXF",
-    coordinates: [13.508611111111, 52.502777777778], // lon, lat
-    destinations: ["LHR", "MEB", "ANC"]
-  },
-  {
-    name: "London (Heathrow)",
-    id: "LHR",
-    coordinates: [-0.45, 51.466666666667], // lon, lat
-    destinations: []
-  },
-  {
-    name: "Melbourne",
-    id: "MEB",
-    coordinates: [144.86666666667, -37.7], // lon, lat
-    destinations: []
-  },
-  {
-    name: "Anchorage",
-    id: "ANC",
-    coordinates: [-149.98333333333, 61.166666666667], // lon, lat
-    destinations: []
-  }
-]
-
-const users = [
-  {
-    id: "P1",
-    visible: true,
-    airport: "SXF"
-  },
-  {
-    id: "P2",
-    visible: true,
-    airport: "MEB"
-  },
-  {
-    id: "P3",
-    visible: true,
-    airport: "MEB"
-  },
-  {
-    id: "P4",
-    visible: false,
-    airport: "MEB"
-  }
-]
-
-const activeUser = "P1"
-
-function App() {
-  const airportMarkers = airports.map(airport => (
-    <Marker coordinates={airport.coordinates}>
-      <circle r={4} fill="#CCC" />
-    </Marker>
-  ))
-
-  const userMarkers = users.filter(user => user.visible).map(user => (
-    <Marker coordinates={airports.find(airport => airport.id === user.airport).coordinates}>
-      <circle r={4} fill={user.id === activeUser ? "#C44D58" : "#4ECDC4"} />
-    </Marker>
-  ))
-
-  const activeUserAirportId = users.find(user => user.id === activeUser).airport
-  const activeUserAirport = airports.find(airport => airport.id === activeUserAirportId)
-  const activeUserDestinations = airports
-    .filter(airport => activeUserAirport.destinations.includes(airport.id))
-  const activeUserDestinationMarkers = activeUserDestinations.map(destination => <Line
-      from={activeUserAirport.coordinates}
-      to={destination.coordinates}
-      stroke="#FF6B6B"
-      strokeWidth={2}
-      strokeLinecap="round"
-    />)
 
 
-  return <>
-    <ComposableMap width={600} height={300} projectionConfig={{ scale: 50 }}  projection="geoMercator" style={{background: "#556270"}}>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map(geo => <Geography key={geo.rsmKey} stroke="none" fill="#AACCB1" geography={geo} />)
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      airports: [
+        {
+          name: "Berlin (Schoenefeld)",
+          id: "SXF",
+          coordinates: [13.508611111111, 52.502777777778], // lon, lat
+          destinations: ["LHR", "MEB", "ANC"]
+        },
+        {
+          name: "London (Heathrow)",
+          id: "LHR",
+          coordinates: [-0.45, 51.466666666667], // lon, lat
+          destinations: ["MEB"]
+        },
+        {
+          name: "Melbourne",
+          id: "MEB",
+          coordinates: [144.86666666667, -37.7], // lon, lat
+          destinations: ["ANC"]
+        },
+        {
+          name: "Anchorage",
+          id: "ANC",
+          coordinates: [-149.98333333333, 61.166666666667], // lon, lat
+          destinations: ["SXF", "MEB"]
         }
-      </Geographies>
-      {activeUserDestinationMarkers}
-      {airportMarkers}
-      {userMarkers}
-    </ComposableMap>
-    <div>
-      {users.map(user => <UserView {...user} active={activeUser === user.id} destinations={activeUser === user.id ? activeUserDestinations : []} />)}
-    </div>
-  </>
+      ],
+      users: [
+        {
+          id: "P1",
+          visible: true,
+          airport: "SXF"
+        },
+        {
+          id: "P2",
+          visible: true,
+          airport: "MEB"
+        },
+        {
+          id: "P3",
+          visible: true,
+          airport: "MEB"
+        },
+        {
+          id: "P4",
+          visible: false,
+          airport: "MEB"
+        }
+      ],
+      activeUser: "P1"
+    }
+  }
+
+  setUserAirport (userId, airportId) {
+    const newState = {
+      ...this.state,
+      users: this.state.users.map(user => user.id === userId ? {...user, airport: airportId} : user)
+    }
+    console.log(newState)
+    this.setState(newState)
+  }
+
+  render () {
+    const { airports, users, activeUser } = this.state
+    const airportMarkers = airports.map(airport => (
+      <Marker coordinates={airport.coordinates}>
+        <circle r={4} fill="#CCC" />
+      </Marker>
+    ))
+
+    const userMarkers = users.filter(user => user.visible).map(user => (
+      <Marker coordinates={airports.find(airport => airport.id === user.airport).coordinates}>
+        <circle r={4} fill={user.id === activeUser ? "#C44D58" : "#4ECDC4"} />
+      </Marker>
+    ))
+
+    const activeUserAirportId = users.find(user => user.id === activeUser).airport
+    const activeUserAirport = airports.find(airport => airport.id === activeUserAirportId)
+    const activeUserDestinations = airports
+      .filter(airport => activeUserAirport.destinations.includes(airport.id))
+    const activeUserDestinationMarkers = activeUserDestinations.map(destination => <Line
+        from={activeUserAirport.coordinates}
+        to={destination.coordinates}
+        stroke="#FF6B6B"
+        strokeWidth={2}
+        strokeLinecap="round"
+      />)
+
+
+    return <>
+      <ComposableMap width={600} height={300} projectionConfig={{ scale: 50 }}  projection="geoMercator" style={{background: "#556270"}}>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => <Geography key={geo.rsmKey} stroke="none" fill="#AACCB1" geography={geo} />)
+          }
+        </Geographies>
+        {activeUserDestinationMarkers}
+        {airportMarkers}
+        {userMarkers}
+      </ComposableMap>
+      <div>
+        {users.map(user => (
+          <UserView
+            {...user}
+            active={activeUser === user.id}
+            destinations={activeUser === user.id ? activeUserDestinations : []}
+            onSelect={airport => this.setUserAirport(user.id, airport)}
+          />
+        ))}
+      </div>
+    </>
+  }
 }
 
 export default App;
--- src/UserView.js
+++ src/UserView.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const UserView = ({id, active, destinations}) => {
+const UserView = ({id, active, destinations, onSelect}) => {
   return <div style={{display: 'inline-block', width: "25%", border: "1px solid black"}}>
     <h1 style={{color: active ? 'red' : 'black'}}>{id}</h1>
     Pick your destination:
-    {destinations.map(destination => <div>{destination.id}</div>)}
+    {destinations.map(destination => <div onClick={() => onSelect(destination.id)}>{destination.id}</div>)}
   </div>
 }
 
